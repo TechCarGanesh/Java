6836def9b8ae5d7475702e03c442ac76ddac5b9d
Ganesh Kumar <154118673+TechCarGanesh@users.noreply.github.com>
2025-02-22 23:58:47 -0800

    Add files via upload

---
+++ Lab13_LList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab13_llist;
+
+/**
+ *
+ * @author pkuma
+ */
+public class Lab13_LList {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
